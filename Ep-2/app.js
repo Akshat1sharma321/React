7d6863df9496c948209ae0c67042a22c7927ed13
Akshat <Akshatsharma5901@gmail.com>
2024-12-25 20:15:49 +0530

    added react basics

--- Ep-2/app.js
+++ Ep-2/app.js
@@ -7,18 +7,21 @@
 // <div class="footer" id="foot">
 //   <h6>Made with love from Akshat</h6>
 // </div>
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const headi = ReactDOM.createRoot(document.getElementById("head"));
 const header = React.createElement("h1", {}, "DAY-2 REACT");
 headi.render(header);
 
-const budi = ReactDOM.createRoot(document.getElementById('bud'));
+const budi = ReactDOM.createRoot(document.getElementById("bud"));
 const bodyText = React.createElement(
   "h2",
   {},
-  "today we are learning about NPM and package managers okay...."
+  "today we are learning about NPM and package managers  okay...."
 );
 budi.render(bodyText);
 
-const footi = ReactDOM.createRoot(document.getElementById('foot'));
+const footi = ReactDOM.createRoot(document.getElementById("foot"));
 const footTxt = React.createElement("h3", {}, "Made with love from Akshat");
 footi.render(footTxt);
